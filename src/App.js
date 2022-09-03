c17e8d39e61c2c125eaec51873b63291c08e5d47
ajiprasetyo471 <ajiprsty4713@gmail.com>
2022-09-03 19:02:51 +0700

    revisi

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar';
+import Details from './components/Details';
+import Forecast from './components/Forecast';
+import getFormattedWeatherData from './services/weatherService';
+import { useEffect, useState } from 'react';
 
 function App() {
+  // const handleLocationClick = () => {
+  //   if (navigator.geolocation) {
+  //     navigator.geolocation.getCurrentPosition((position) => {
+  //       let lat = position.coords.latitude;
+  //       let lon = position.coords.longitude;
+
+  //       setQuery(`${lat}%2C${lon}`);
+  //     });
+  //   }
+  // };
+  const [query, setQuery] = useState('kendal');
+  const [units, setUnits] = useState('metric');
+  const [weather, setWeather] = useState(null);
+
+  useEffect(() => {
+    const fetchWeather = async () => {
+      await getFormattedWeatherData(query).then((data) => {
+        setWeather(data);
+      });
+    };
+
+    // let lon, lat;
+    // navigator.geolocation.getCurrentPosition((position) => {
+    //   lon = position.coords.longitude;
+    //   lat = position.coords.latitude;
+    //   fetchWeather({ ...lon, ...lat });
+    // });
+    fetchWeather();
+  }, [query, units]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App  pb-10">
+      {weather && (
+        <div>
+          <NavBar setQuery={setQuery} units={units} setUnits={setUnits} />
+          <Details weather={weather} />
+          <Forecast items={weather.hourly} title="Hourly Forecast" />
+          <Forecast items={weather.daily} title="Daily Forecast" />
+        </div>
+      )}
     </div>
   );
 }
